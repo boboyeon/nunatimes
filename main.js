8d6a81c1c41d899879c04042358ec205311dfebb
boyeon kim <hajoce02@naver.com>
2024-07-10 03:02:56 +0900

    third commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // const API_KEY = '59fb8bf8b6e54f2aa77e0eb21f46841b'
 let news=[]
 const getLatestNews = async () => {
-    const url = new URL(`https://bobotimes.netlify.app/top-headlines?country=kr`);
+    const url = new URL(`https://bobotimes.netlify.app/top-headlines`);
     const response = await fetch(url);
     // await : 비동기함수, 반드시 async함수에서만 사용가능
     const data = await response.json();
