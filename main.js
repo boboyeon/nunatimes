92473e57acd5533bc85c0521f673a7dc04e958aa
boyeon kim <hajoce02@naver.com>
2024-07-12 23:42:15 +0900

    14th commit

--- main.js
+++ main.js
@@ -1,92 +1,155 @@
 // const API_KEY = '59fb8bf8b6e54f2aa77e0eb21f46841b'
 let newsList = [];
-const menus = document.querySelectorAll(".menus_btn button"); 
+const menus = document.querySelectorAll(".menus_btn button");
 // 메뉴 버튼 호출
-menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)));
+const searchInput = document.getElementById("search-input");
+// input 호출
+const navClick = document.querySelectorAll('.menus > button');
+// nav 버튼 호출
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 // 각 버튼에 클릭이벤트
+let url = new URL(
+  `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
+);
+// 매번 반복되어 사용되지만 각 함수마다 다른 형태로 쓰이기때문에 let으로 전역변수 설정 후 각 함수 안에서 재정의
 
-async function getLatestNews() {
-    let url = new URL(
-        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
-      );
+// 반복되는 코드 리펙토링
+const getNews = async () => {
+  try {
     const response = await fetch(url);
     // await : 비동기함수, 반드시 async함수에서만 사용가능
+
     const data = await response.json();
     // json : 파일 형식 중 하나, 객체를 텍스트화 시킨 파일
-    newsList = data.articles;
-    render();
-    console.log("ddd", newsList);
-}
+    if (response.status === 200) {
+      // response/ status 정보가 200이면
+      if(data.articles.length===0){
+        // 검색 결과인 data.article의 길이(갯수)가 0이면
+        throw new Error("검색어에 맞는 결과가 없습니다.")
+      }
+      newsList = data.articles;
+      render(); // 뉴스 정상 출력
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    errorRender(error.message); 
+    // error상황에서 에러 메세지를 errorRender의 매개변수로 전달 (errorMessage)
+  }
+};
+
+const getLatestNews = async () => {
+  url = new URL( // 재정의 된 url
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
+  );
+ await getNews();
+};
 
 // 메뉴 카테고리 버튼 클릭 이벤트 함수
-const getNewsByCategory= async (event)=>{
-    const category = event.target.textContent.toLowerCase();
-    console.log("category",category);
-    const url = new URL(
-        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
-      );
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("ddd",data);
-    newsList = data.articles;
-    render();
-}
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
+  );
+  await getNews();
+
+  if (window.innerWidth <= 768) {
+    closeNav();
+  } // 모바일에서 카테고리 선택시 sideNav 닫기
+
+};
 
 // 검색 함수
-const searchNews= async ()=>{
-    const keyword = document.getElementById("search-input").value
-    console.log("keyword", keyword);
-    if (!keyword.trim()) return;
-    const url = new URL(
-        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`
-      );
-      
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("keyword data",data);
-    newsList = data.articles;
-    render();
-    document.getElementById("search-input").value = '';
-}
+const searchNews = async () => {
+  const keyword = document.getElementById("search-input").value;
+  if (!keyword.trim()) return;
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`
+  );
 
-document.getElementById("search-input").addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        searchNews(); // 엔터 키가 눌렸을 때 검색 수행
-    }
+  await getNews();
+
+  document.getElementById("search-input").value = "";
+};
+
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    searchNews(); 
+    // 엔터 키가 눌렸을 때 검색 수행
+  }
 });
 
+searchInput.addEventListener("focus", () => {
+  searchInput.value = ""; 
+  // 포커스가 주어졌을 때 입력 값 비우기
+});
+
+// 키워드를 강조하는 함수
+function highlightKeyword(keyword, txt) {
+  if (!keyword) return txt;
+  let index = txt.toLowerCase().indexOf(keyword.toLowerCase());
+  while (index !== -1) {
+    const start = '<span class="light_blue">';
+    const end = '</span>';
+    txt =
+      txt.slice(0, index) +
+      start +
+      txt.slice(index, index + keyword.length) +
+      end +
+      txt.slice(index + keyword.length, txt.length);
+    index = txt.toLowerCase().indexOf(keyword.toLowerCase(), index + start.length + keyword.length);
+  }
+  return txt;
+}
+
 const render = () => {
-  const newsHTML = newsList.map(
-    (news) => `<div class="row news">
+  const keyword = document.getElementById("search-input").value.trim();
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
                 <!-- .row : 부트스트랩 grid system class -->
                 <!-- 하위 div가 전부 일렬로 정렬됨 (row:세로)-->
                 <div class="col-lg-4">
                   <!-- col : 가로, lg : large,size, 4 : 비율 -->
                   <img
                     class="news_img_size"
-                    src="${news.urlToImage || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'}"
+                    src="${
+                      news.urlToImage ||
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+                    }"
                     onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';"
                   />
                 </div>
                 <div class="col-lg-8">
                   <!-- col : 가로, lg : large,size, 8 : 비율 -->
                   <!-- 1줄에 칼럼 사이즈가 8:4 비율인 그리드 형태 -->
-                  <h2>${news.title}</h2>
+                  <!-- 제목과 설명에 highlightKeyword 함수 적용 -->
+                  <h2>${highlightKeyword(keyword, news.title)}</h2>
                   <p>${
                     news.description == null || news.description == ""
                       ? "내용없음"
                       : news.description.length > 200
-                      ? news.description.substring(0, 200) + "..."
-                      : news.description
-                   }</p>
+                      ? highlightKeyword(keyword, news.description.substring(0, 200)) + "..."
+                      : highlightKeyword(keyword, news.description)
+                  }</p>
                   <div>${news.source.name || "no source"}  ${moment(
-                news.publishedAt
-     ).fromNow()}</div>
+        news.publishedAt
+      ).fromNow()}</div>
             </div>
           </div>`
-  ).join('');
-  console.log("HTML",newsHTML);
+    )
+    .join("");
   document.getElementById("news_board").innerHTML = newsHTML;
+};
+
+// 에러 메세지 출력함수
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+                      ${errorMessage}
+                     </div>`;
+                     document.getElementById("news_board").innerHTML=errorHTML
 };
 
 getLatestNews();
@@ -96,13 +159,23 @@ getLatestNews();
 // 3. 그 뉴스들 보여주기
 
 const openSearchBox = () => {
-    let inputArea = document.getElementById("input_area");
-    if (inputArea.style.display === "inline") {
-      inputArea.style.display = "none";
-    } else {
-      inputArea.style.display = "inline";
-    }
-  };
+  let inputArea = document.getElementById("input_area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
+navClick.forEach((button) => {
+  button.addEventListener('click', () => {
+    // 모든 버튼에서 nav_tabs 클래스를 제거
+    navClick.forEach((btn) => btn.classList.remove('nav_tabs'));
+    // 클릭된 버튼에 nav_tabs 클래스를 추가
+    button.classList.add('nav_tabs');
+  });
+});
+
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
